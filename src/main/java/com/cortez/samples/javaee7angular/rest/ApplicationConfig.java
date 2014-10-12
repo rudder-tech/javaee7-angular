b6dadb7343afb6255d00e16e978995c6548b8ad6
jasonwillix <jason.willis@rudder-tech.com>
2014-10-12 14:37:10 -0300

    Add Swagger support for API documentation

---
+++ src/main/java/com/cortez/samples/javaee7angular/rest/ApplicationConfig.java
@@ -0,0 +1,30 @@
+package com.cortez.samples.javaee7angular.rest;
+
+import javax.ws.rs.core.Application;
+import java.util.Set;
+
+@javax.ws.rs.ApplicationPath("rest")
+public class ApplicationConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        resources.add(com.wordnik.swagger.jaxrs.listing.ApiListingResource.class);
+        resources.add(com.wordnik.swagger.jaxrs.listing.ApiDeclarationProvider.class);
+        resources.add(com.wordnik.swagger.jaxrs.listing.ApiListingResourceJSON.class);
+        resources.add(com.wordnik.swagger.jaxrs.listing.ResourceListingProvider.class);
+        addRestResourceClasses(resources);
+        return resources;
+    }
+
+    /**
+     * Do not modify addRestResourceClasses() method.
+     * It is automatically populated with
+     * all resources defined in the project.
+     * If required, comment out calling this method in getClasses().
+     */
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(PersonResource.class);
+    }
+
+}
